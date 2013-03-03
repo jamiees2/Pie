4aef4bc6f12325bd69984eae9971d28b533fa380
Hlynur Óskar Guðmundsson <runylh@gmail.com>
2013-03-03 12:50:31 +0000

    Added Some Shapes With Characters

---
+++ Pie/CharShapes/Square.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pie.CharShapes
+{
+    public class Square
+    {
+        public void FSquare(int size, char symbol)//Filled Square
+        {
+            for (int i = 0; i < size; i++)
+            {
+            System.Console.WriteLine(symbol.ToString().PadLeft(size, symbol));//Fill the square
+            }
+        }
+
+        public void HSquare(int size, char symbol)//Hollow Square
+        {
+            System.Console.WriteLine(symbol.ToString().PadLeft(size, symbol));//Top of square
+            for (int i = 0; i < (size - 2); i++)
+            {
+                System.Console.WriteLine(symbol + symbol.ToString().PadLeft(size - 1, ' '));
+            }
+            if (size != 1)
+                System.Console.WriteLine(symbol.ToString().PadLeft(size, symbol));//Bottom of square
+        }
+
+        public void FXYSquare(int x, int y, char symbol)//Filled Different width an height square
+        {
+            for (int i = 0; i < y; i++)
+            {
+                System.Console.WriteLine(symbol.ToString().PadLeft(x, symbol));
+            }
+        }
+
+        public void HXYSquare(int x, int y, char symbol)//Hollow Different width and height square
+        {
+            System.Console.WriteLine(symbol.ToString().PadLeft(x, symbol));
+            for (int i = 0; i < (y-2); i++)
+            {
+                if(x!=1)
+                System.Console.WriteLine(symbol+symbol.ToString().PadLeft(x-1, symbol));
+                else
+                    System.Console.WriteLine(symbol);
+            }
+            if(y!=1)
+                System.Console.WriteLine(symbol.ToString().PadLeft(x, symbol));
+        }
+    }
+}
---
+++ Pie/CharShapes/Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pie.CharShapes
+{
+    public class Triangle
+    {
+        public void FTriangle(int size, char symbol)//Filled Triangle
+        {
+            System.Console.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            for (int i = 1; i < size; i++)
+            {
+                System.Console.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                System.Console.WriteLine(symbol.ToString().PadLeft(i * 2, symbol));//Print symbol
+            }
+        }
+
+        public void HTriangle(int size, char symbol)//Hollow Triangle
+        {
+            System.Console.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            for (int i = 1; i < size-1; i++)
+            {
+                System.Console.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                System.Console.WriteLine(symbol.ToString().PadLeft(i * 2, ' '));//Print symbol
+            }
+            System.Console.WriteLine(symbol.ToString().PadLeft((size*2)-1, symbol));//Triangle base
+        }
+
+        public void HalfFTriangle(int size, char symbol)//Half Filled Triangle
+        {
+            System.Console.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            for (int i = 1; i < size; i++)
+            {
+                System.Console.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                System.Console.WriteLine(symbol.ToString().PadLeft(i, symbol));//Print symbol
+            }
+        }
+
+        public void HalfHTriangle(int size, char symbol)//Half Hollow Triangle
+        {
+            System.Console.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            for (int i = 1; i < size - 1; i++)
+            {
+                System.Console.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                System.Console.WriteLine(symbol.ToString().PadLeft(i, ' '));//Print symbol
+            }
+            System.Console.WriteLine(symbol.ToString().PadLeft(size, symbol));//Triangle base
+        }
+    }
+}
