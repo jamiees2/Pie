3fe3d0b79f8c177118df058c452ce85b3925b6c6
James Elías <jamiees2@gmail.com>
2013-03-05 18:33:20 +0000

    Added code to the FKTemplate project

--- FKTemplate/Program.cs
+++ FKTemplate/Program.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
+using Pie;
+using Pie.Strings;
+using Pie.Math;
+using Pie.Math.Geometry;
+using Pie.Time;
+using Pie.Units;
+using Pie.IO;
+using Pie.FK;
 
-namespace FKTemplate
+namespace $safeprojectname$
 {
+    class FK$year$ : FKController
+    {
+        public FK$year$(string title = null, string[] args = null) : base(title, args) {}
+
+        [FKAction("1", "Problem 1")]
+        public void Problem1()
+        { 
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            (new FKProgram("FK$year$", args)).Run();
         }
     }
 }
