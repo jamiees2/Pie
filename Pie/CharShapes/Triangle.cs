a8f45812c7cb0d8dcc213d1b4006a391e2984c3a
RuNnNy <runylh@gmail.com>
2013-03-09 15:22:32 +0000

    Converted to string array

--- Pie/CharShapes/Triangle.cs
+++ Pie/CharShapes/Triangle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,93 +11,102 @@ namespace Pie.CharShapes
     {
         public void FTriangle(int size, char symbol)//Filled Triangle
         {
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
             for (int i = 1; i < size; i++)
             {
-                PieConsole.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i * 2, symbol));//Print symbol
+                builder.Append(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i * 2, symbol));//Print symbol
             }
+            return builder.ToString();
         }
         
         public void FTriangleSpace(int size, char symbol)//Filled Triangle with spaces
         {
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
             for (int i = 1; i < size; i++)
             {
-                PieConsole.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
-                PieConsole.WriteLine(Regex.Replace(symbol.ToString().PadLeft(i, symbol), "\" + symbol, " "+symbol));
+                builder.Append(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                builder.AppendLine(Regex.Replace(symbol.ToString().PadLeft(i, symbol), "\" + symbol, " "+symbol));
             }
         }
         
         public void HTriangle(int size, char symbol)//Hollow Triangle
         {
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
             for (int i = 1; i < size-1; i++)
             {
-                PieConsole.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i * 2, ' '));//Print symbol
+                builder.Append(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i * 2, ' '));//Print symbol
             }
-            PieConsole.WriteLine(symbol.ToString().PadLeft((size*2)-1, symbol));//Triangle base
+            builder.AppendLine(symbol.ToString().PadLeft((size*2)-1, symbol));//Triangle base
         }
         
         public void HalfFTriangle(int size, char symbol)//Half Filled Triangle
         {
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
             for (int i = 1; i < size; i++)
             {
-                PieConsole.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i, symbol));//Print symbol
+                builder.Append(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i, symbol));//Print symbol
             }
         }
         
         public void HalfFTriangleR(int size, char symbol)//Half Filled Triangle Reversed
         {
+            StringBuilder builder = new StringBuilder();
             for (int i = 1; i < size; i++)
             {
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i, symbol));//Print symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i, symbol));//Print symbol
             }
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
         }
         
         public void HalfFTriangleRR(int size, char symbol)//Half Filled Triangle Reversed Reversed... Don't ask
         {
+            StringBuilder builder = new StringBuilder();
             for (int i = size; i < 0; i--)
             {
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i, symbol));//Print symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i, symbol));//Print symbol
             }
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
         }
         
         public void HalfHTriangle(int size, char symbol)//Half Hollow Triangle
         {
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
             for (int i = 1; i < size - 1; i++)
             {
-                PieConsole.Write(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i, ' '));//Print symbol
+                builder.Append(symbol.ToString().PadLeft((size - i), ' '));//Whitespace before printing symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i, ' '));//Print symbol
             }
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, symbol));//Triangle base
+            builder.AppendLine(symbol.ToString().PadLeft(size, symbol));//Triangle base
         }
         
         public void HalfHTriangleR(int size, char symbol)//Half Hollow Triangle Reversed
         {
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Top of triangle
             for (int i = 1; i < size - 1; i++)
             {
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i, ' '));//Print symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i, ' '));//Print symbol
             }
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, symbol));//Triangle base
+            builder.AppendLine(symbol.ToString().PadLeft(size, symbol));//Triangle base
         }
         
         public void HalfHTriangleRR(int size, char symbol)//Half Hollow Triangle Reversed Reversed.. Don't ask
         {
-            
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, symbol));//Triangle base... on top
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(symbol.ToString().PadLeft(size, symbol));//Triangle base... on top
             for (int i = size; i < 0; i--)
             {
-                PieConsole.WriteLine(symbol.ToString().PadLeft(i, ' '));//Print symbol
+                builder.AppendLine(symbol.ToString().PadLeft(i, ' '));//Print symbol
             }
-            PieConsole.WriteLine(symbol.ToString().PadLeft(size, ' '));//Bottom of triangle
+            builder.AppendLine(symbol.ToString().PadLeft(size, ' '));//Bottom of triangle
         }
     }
 }
