1c836f96507d8b364b12b5db8b49fc84ba37db6a
RuNnNy <runylh@gmail.com>
2013-03-09 16:35:12 +0000

    Changed everything to static
    again...............

--- Pie/Math/Geometry/Formulas/Triangle.cs
+++ Pie/Math/Geometry/Formulas/Triangle.cs
@@ -7,17 +7,17 @@ namespace Pie.Math.Geometry.Formulas
 {
     public class Triangle
     {
-        public double Area(int b, int h)
+        public static double Area(int b, int h)
         {
             return (1 / 2) * (b * h);
         }
 
-        public double Perimeter(int a, int b, int c)
+        public static double Perimeter(int a, int b, int c)
         {
             return a + b + c;
         }
 
-        public double Pythagoras(int a, int b)
+        public static double Pythagoras(int a, int b)
         {
             return System.Math.Sqrt(System.Math.Pow(a, 2) + System.Math.Pow(b, 2));
         }
